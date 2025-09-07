ccaf9ff099fc2393676494be3250acb67da5bdf0
nikitoid <neekeetoeed@gmail.com>
2025-09-08 01:32:38 +0400

    work in progress

--- js/app.js
+++ js/app.js
@@ -8,29 +8,21 @@ if ('serviceWorker' in navigator) {
   let newWorker;
 
   const updateBanner = document.getElementById('update-banner');
-  const updateButton = document.getElementById('update-button');
-  const closeUpdateBannerButton = document.getElementById('close-update-banner');
 
   function showUpdateBanner() {
     if (updateBanner) {
-      updateBanner.classList.remove('hidden');
-      setTimeout(() => updateBanner.classList.add('show'), 10);
+      updateBanner.classList.remove('hidden', 'animate-out');
+      updateBanner.classList.add('animate-in');
+      setTimeout(hideUpdateBanner, 5000);
     }
   }
 
   function hideUpdateBanner() {
     if (updateBanner) {
-      updateBanner.classList.remove('show');
+      updateBanner.classList.remove('animate-in');
+      updateBanner.classList.add('animate-out');
     }
   }
-  
-  updateButton.addEventListener('click', () => {
-    console.log('APP: Пользователь нажал "Обновить". Отправка сообщения SKIP_WAITING новому Service Worker.');
-    newWorker.postMessage({ type: 'SKIP_WAITING' });
-    hideUpdateBanner();
-  });
-
-  closeUpdateBannerButton.addEventListener('click', hideUpdateBanner);
   
   console.log('APP: Регистрация Service Worker...');
   navigator.serviceWorker.register('/sw.js').then(reg => {
@@ -41,16 +33,18 @@ if ('serviceWorker' in navigator) {
       newWorker.addEventListener('statechange', () => {
         console.log('APP: Состояние нового Service Worker изменилось:', newWorker.state);
         if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-          console.log('APP: Новая версия установлена, ожидание активации.');
+          console.log('APP: Новая версия установлена, показываем уведомление и начинаем активацию.');
           showUpdateBanner();
+          newWorker.postMessage({ type: 'SKIP_WAITING' });
         }
       });
     });
 
     if (reg.waiting) {
-        console.log('APP: Обнаружен ожидающий Service Worker. Показ баннера обновления.');
+        console.log('APP: Обнаружен ожидающий Service Worker. Показ баннера и активация.');
         newWorker = reg.waiting;
         showUpdateBanner();
+        newWorker.postMessage({ type: 'SKIP_WAITING' });
     }
   }).catch(registrationError => {
     console.log('SW registration failed: ', registrationError);
