cdc87fa0ba596398ac78cd02291f88b9250ea919
nikitoid <neekeetoeed@gmail.com>
2025-09-08 02:47:25 +0400

    work in progress

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Меняем версию кэша, чтобы спровоцировать обновление
-const CACHE_NAME = "randomatched-cache-v9";
+const CACHE_NAME = "randomatched-cache-v10";
 const urlsToCache = [
   "/",
   "/index.html",
