d4d83b7cc8809012326dddbb5fa861ad4f66d975
nikitoid <neekeetoeed@gmail.com>
2025-10-07 01:02:36 +0400

    add firebase-config for local tests

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Этот файл содержит конфигурацию Firebase и не должен попадать в систему контроля версий.
+// Скопируйте реальные данные вашего проекта из консоли Firebase.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSvSbR_NJj7riu0HZPz3nile1X4tuxfsI",
+  authDomain: "unmatched-randomizer.firebaseapp.com",
+  projectId: "unmatched-randomizer",
+  storageBucket: "unmatched-randomizer.firebasestorage.app",
+  messagingSenderId: "168086799887",
+  appId: "1:168086799887:web:3c8af51f935999b7d6c57a",
+  measurementId: "G-GEQPMK68B0",
+};
