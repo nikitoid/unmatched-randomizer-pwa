0dfd6992f3f7f0a8e297afe148a14050494808ff
nikitoid <neekeetoeed@gmail.com>
2025-10-18 15:20:11 +0400

    fix toast for add new lists from cloud

--- js/modules/listManager.js
+++ js/modules/listManager.js
@@ -87,8 +87,22 @@ const ListManager = {
    * @param {CustomEvent} event - Событие с актуальным состоянием списков.
    */
   handleExternalUpdate(event) {
-    const { lists } = event.detail;
-    this.updateLists(lists);
+    const { lists: newLists } = event.detail;
+    const oldListNames = Object.keys(this.heroLists);
+
+    Object.keys(newLists).forEach((listName) => {
+      const isNew = !oldListNames.includes(listName);
+      const isCloud = newLists[listName].type === "cloud";
+
+      if (isNew && isCloud) {
+        Toast.success(
+          `Список "${listName}" был добавлен`,
+          `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z"/></svg>`
+        );
+      }
+    });
+
+    this.updateLists(newLists);
   },
 
   /**
